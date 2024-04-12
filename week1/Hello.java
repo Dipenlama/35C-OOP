9fcde03224af77ffdbdebfee6d31e84a30d0d0c5
Dipen <dipenlama9937@gmail.com>
2024-04-12 12:27:06 +0545

    week 1 task 1 question commented

--- week1/Hello.java
+++ week1/Hello.java
@@ -2,6 +2,8 @@ package Tasks.week1;
 
 public class Hello {
     public static void main(String[] args) {
+        
+        /*Write a program that prints Hello World! to  the console. Save the program as Hello.java.*/
         System.out.println("Hello World!");
     }
     
