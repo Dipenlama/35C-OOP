b2f54441a10acc9f36bf19c88876374927fa367f
Dipen <dipenlama9937@gmail.com>
2024-04-15 07:24:06 +0545

    week 1 task 5 question added

--- week1/Hello.java
+++ week1/Hello.java
@@ -13,7 +13,7 @@ public class Hello {
 
             System.out.print (not println) and produces the same output.*/
         System.out.print("This is my first program \n");
-        System.out.print("I am on module ST4003CEM");
+        System.out.println("I am on module ST4003CEM");
         
         /*3. Write a program that prints the following output:
 
@@ -31,13 +31,45 @@ public class Hello {
 
             '' is not the same as "*/
 
-            System.out.print("A \"quoted\" String is \n");
-            System.out.print("\'much\' better if you learn \n");
-            System.out.print("the rules of \"escape sequences.\"\n");
-            System.out.print("Also, \"\" represents an empty\n");
-            System.out.print("String. Don't forget: use \\\"\n");
+            System.out.println("A \"quoted\" String is ");
+            System.out.println("\'much\' better if you learn ");
+            System.out.println("the rules of \"escape sequences.\"");
+            System.out.println("Also, \"\" represents an empty");
+            System.out.println("String. Don't forget: use \\\"");
             System.out.println("instead of \"!");
-            System.out.print("''is not the same as \"*/");
+            System.out.println("''is not the same as \"*/");
+
+           /*4. WAP that prints the following pattern:
+
+                *
+
+                **
+
+                ***
+
+                ****  */
+            System.out.println("*");
+            System.out.println("**");
+            System.out.println("***");
+            System.out.println("****");
+
+
+           /*  5. WAP prints the following pattern:
+
+                *********
+
+                *********
+
+                *********
+
+                ********* */
+
+            System.out.println("********");
+            System.out.println("********");
+            System.out.println("********");
+            System.out.println("********");
+
+            
             }
     
 }
