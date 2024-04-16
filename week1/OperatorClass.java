2ee7db8bc4e05476ee7d13199bc9ea4d492cb777
Dipen <dipenlama9937@gmail.com>
2024-04-16 12:26:19 +0545

    operator file added

---
+++ week1/OperatorClass.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class OperatorClass {
+    public static void main(String[] args) {
+        //Arithmetic Operation
+        int a=10,b=20;
+        int sum=a+b; //here + is an arithmetic Operation
+        System.out.println("the sum is "+sum);
+
+        System.out.println("the difference is "+ (a-b));
+        System.out.println("the multiplication is"+(a*b));
+        System.out.println("the division is "+(a/b));
+        System.out.println("the modulo is "+ (a%b));
+    }
+    
+}
