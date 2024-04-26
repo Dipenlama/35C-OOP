e3250009cca98316b4cf63709833f38ae7a2d88b
Dipen <dipenlama9937@gmail.com>
2024-04-26 21:54:06 +0545

    week 3 task added

--- weeek3Task.java
+++ weeek3Task.java
@@ -6,12 +6,54 @@ public class weeek3Task {
         Scanner scan=new Scanner (System.in);
         //1. Write a JAVA program to find the maximum between three numbers.
 
-        System.out.println("Enter first number:");
-        double firstNum=scan.nextDouble();
-        System.out.println("Enter second number:");
-        double secondNum=scan.nextDouble();
-        System.out.println("Enter third number:");
-        double thirdNum=scan.nextDouble();
-        
+        // System.out.println("Enter first number:");
+        // double firstNum=scan.nextDouble();
+        // System.out.println("Enter second number:");
+        // double secondNum=scan.nextDouble();
+        // System.out.println("Enter third number:");
+        // double thirdNum=scan.nextDouble();
+
+        // if (firstNum>secondNum && secondNum>thirdNum){
+        //     System.out.println(firstNum+"is greater");
+        // }else if (secondNum>thirdNum && thirdNum>firstNum){
+        //     System.out.println(secondNum+"is greater");
+        // }else {
+        //     System.out.println(thirdNum+"is greater");
+        // }
+
+        //2. Write a JAVA program to check whether a number is negative, positive, or zero.
+        // System.out.println("Enter a number:");
+        // double numb=scan.nextDouble();
+        // if (numb>0){
+        //     System.out.println(numb+"is positive number");
+        // }
+        // else if (numb<0){
+        //     System.out.println(numb+"is negative number");
+        // }
+        // else{
+        //     System.out.println(numb +"is zero");
+        // }
+
+        //3. Write a JAVA program to check whether a number is divisible by 5 and 11 or not.
+        // System.out.println("Enter a number");
+        // int numb1=scan.nextInt();
+        // if (numb1%5==0 && numb1%11==0){
+        //     System.out.println(numb1+" is divisible by 5 and 11");
+        // }else{
+        //     System.out.println(numb1+" is not divisible by 5 and 11");
+        // }
+
+        //4. Write a JAVA program to check whether a number is even or odd.
+        // System.out.println("Enter a number:");
+        // int numb2=scan.nextInt();
+        // if (numb2%2==0){
+        //     System.out.println(numb2+" is even number");
+
+        // }else{
+        //     System.out.println(numb2+ " is odd number");
+        // }
+
+        //5. Write a JAVA program to check whether a year is a leap year or not. 
+
     }
 }
