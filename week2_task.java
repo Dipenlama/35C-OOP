2b70bba6d95528a9add277deb9236268704c8fb5
Dipen <dipenlama9937@gmail.com>
2024-04-26 12:51:35 +0545

    week 2 task added

---
+++ week2_task.java
@@ -0,0 +1,131 @@
+package Tasks;
+import java.util.Scanner;
+
+public class week2_task {
+    public static void main(String[] args) {
+        //1. Write a program to check whether a person can cast a vote or not. The condition is you must be over 18 years old to vote.
+
+        Scanner scan=new Scanner(System.in);
+        // System.out.println("Enter your age: ");
+        // int age=scan.nextInt();
+
+        // if (age<=18){
+        //     System.out.println("You are not able to vote...");
+        // }else if (age>18){
+        //     System.out.println("You can vote...");
+        // }
+
+        
+
+        //2. Write a program to calculate SI. 
+        //Formula Simple Interest = (PrincipleAmount*Time*Rate/100);
+
+        // System.out.println("Enter the principle: ");
+        // int principle=scan.nextInt();
+
+        // System.out.println("Enter the time:");
+        // double time=scan.nextInt();
+
+        // System.out.println("Enter the rate:");
+        // double rate=scan.nextInt();
+
+        // System.out.println("simple interest: "+ principle*time*rate/100);
+
+        // scan.close();
+
+
+
+        //3. Write a program to calculate, area of the triangle, and the Volume of the Cube and Cuboid.
+        //Formula: Volume of Cuboid = length*width*height; and Volume of Cube: side*side*side;
+
+    
+            //for cuboid:
+        // System.out.println("Enter the length of cuboid:");
+        // int l=scan.nextInt();
+
+        // System.out.println("Enter width of the cuboid:");
+        // int w=scan.nextInt();
+
+        // System.out.println("Enter height of the cuboid:");
+        // int h=scan.nextInt();
+
+        // System.out.println("The area of cuboid is: "+ l*w*h);
+
+        //     for cube:
+        
+        // System.out.println("enter the length:");
+        // int l=scan.nextInt();
+        
+        // System.out.println("The area of cube is "+l*l*l);
+
+            //for triangle:
+        
+        // System.out.println("enter the base of triangle: ");
+        // double b=scan.nextDouble();
+        // System.out.println("Enter the height of triangle: ");
+        // double  h=scan.nextDouble();
+
+        // System.out.println("The area of triangle is "+ 0.5*b*h);
+
+
+        //4. Write the ternary operator for question no. 1
+        // System.out.println("Enter your age: ");
+        // int ages=scan.nextInt();
+
+        // //int exp= (ages>18);
+        // String output=ages>18 ? "you are able to voting" : "you are not able to voting";
+        // System.out.println(output);
+
+
+        //5. Write a program to take two integer inputs from the user and print the sum and product of them.
+            // System.out.println("Enter first number: ");
+            // int first=scan.nextInt();
+            // System.out.println("Enter second number: ");
+            // int second=scan.nextInt();
+
+            // //sum:
+            // System.out.println("The sum of two numbers is " +(first+second));
+            // System.out.println("The product of two number is "+ first*second);
+
+            
+            //6. Take two integer inputs from the user. First, calculate the sum of two, then the product of two. 
+            //Finally, calculate the division of the thus obtained sum and product and print the result.
+
+            // System.out.println("Enter first number: ");
+            // int firstNum=scan.nextInt();
+            // System.out.println("Enter second number: ");
+            // int secondNum=scan.nextInt();
+
+            // double sum2=(firstNum+secondNum);
+            // double prod2=(firstNum*secondNum);
+
+            // System.out.println("the sum of two number is:"+ (sum2));
+            // System.out.println("the product of two number is : "+(prod2));
+
+            // double division=(sum2/prod2);
+            // System.out.println("The division of obtained sum and product is:"+ division);
+
+
+
+            // 7. Take the name, roll number, and field of interest from the user 
+            // and print in the format below: Hey, my name is XYZ and my roll number is XYZ. My field of interest are xyz.
+            // System.out.println("Enter your name:");
+            // String name=scan.nextLine();
+            // System.out.println("Enter your roll number:");
+            // int rollNo=scan.nextInt();
+            // scan.nextLine();
+            // System.out.println("Enter your field of interest: ");
+            // String interest=scan.nextLine();
+
+            // System.out.printf("Hey, my name is %s and my roll number is %d.",name,rollNo);
+            // System.out.printf("My field of interes are %s",interest);
+
+            //8. Take side of a square from user and print area and perimeter of it.
+            // Also, calculate Simple Interest, Area of triangle and Volume of Cube and Cuboid. Take the attributes as user input.
+
+
+    }
+    
+    
+
+}
